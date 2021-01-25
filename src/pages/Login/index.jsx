fdfda6ffaeff41c979cbf07b068e5cc2b86b91ee
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2021-01-25 09:41:44 +0800

    feat: 增加login页面以及路由

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import { createElement } from 'rax';
+import View from 'rax-view';
+import Text from 'rax-text';
+import Logo from '@/components/Logo';
+
+export default function Home() {
+  return (
+    <View >
+      <Text>这里是login页面</Text>
+    </View>
+  );
+}
